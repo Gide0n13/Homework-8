bb14eecfb2d81ef07dd1a52e7110469178471646
Sergey <tallon300@yandex.ru>
2020-06-09 18:13:32 +0300

    Первый коммит

---
+++ NewService/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using NewService.Models;
+
+namespace NewService.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        ProductContext db = new ProductContext();
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = db.Products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+    }
+}
